701abf9a9571ebb105fdb23189fa7590e3b05912
Arghyadeep Ghosh <60299552+Arghya721@users.noreply.github.com>
2023-10-23 21:05:20 +0000

    Fixed terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,23 +11,20 @@ provider "google" {
 
 data "google_project" "project" {}
 
-resource "google_cloud_run_v2_service" "google-vision" {
+resource "google_cloud_run_service" "vision" {
     name     = "vision"
     location = var.region
 
     template {
-        containers {
-            image = "${var.artifact_registry_url}/google-vision:latest"
-            ports {
-                container_port = 1323
+        spec {
+            containers {
+                image = "${var.artifact_registry_url}/google-vision:latest"
+                ports {
+                    container_port = 1323
+                }
             }
         }
     }
-
-    traffic {
-        type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-        percent = 100
-  }
 }
 
 data "google_iam_policy" "noauth" {
@@ -40,9 +37,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-    location = google_cloud_run_v2_service.google-vision.location
-    project  = google_cloud_run_v2_service.google-vision.project
-    service  = google_cloud_run_v2_service.google-vision.name
+    location = google_cloud_run_service.vision.location
+    project  = google_cloud_run_service.vision.project
+    service  = google_cloud_run_service.vision.name
 
     policy_data = data.google_iam_policy.noauth.policy_data
 }
